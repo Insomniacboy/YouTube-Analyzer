f4f3af4def02e7266f602becb322f5ad7394b854
Temirbek <atabekov.t.a@gmail.com>
2023-06-15 14:14:02 +0600

    Added date field

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ emoji_pattern = re.compile("["
         u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
                            "]+", flags=re.UNICODE)
 
+def print_progress_bar(nProcessed, nTotal, barLength=50):
+    percent = float(nProcessed) / nTotal
+    arrow = '-' * int(round(percent * barLength)-1) + '>'
+    spaces = ' ' * (barLength - len(arrow))
+    print('Progress: [%s%s] %d %%' % (arrow, spaces, percent*100), end='\r')
+
 # Main function
 if __name__ == '__main__':
     # set locale
@@ -26,24 +32,26 @@ if __name__ == '__main__':
     if os.path.exists('sample_size.txt'):
         with open('sample_size.txt', 'r') as f:
             sample_size = int(f.read())
+    
+    # create nProcessed and nTotal
+    nProcessed = 0
+    nTotal = 0
+    with open('channels.txt', 'r') as f:
+        nTotal = len(f.readlines())
 
     # read from channels.txt and create object for each channel
     channels = []
     with open('channels.txt', 'r') as f:
         for line in f:
             channels.append(YouTube(line.strip(), sample_size))
-
-    # print each channel
-    # for channel in channels:
-    #     print(channel)
-    #     for video in channel.videos:
-    #         print(video)
+            nProcessed += 1
+            print_progress_bar(nProcessed, nTotal)
 
     # Write to Excel file
-    csv_columns = ['Channel', 'Video', 'Views', 'Likes', 'URL']
+    csv_columns = ['Channel', 'Video', 'Views', 'Likes', 'URL', 'Date']
     with open('output.csv', 'w') as f:
         writer = csv.DictWriter(f, fieldnames=csv_columns)
         writer.writeheader()
         for channel in channels:
             for video in channel.videos:
-                writer.writerow({'Channel': channel.author, 'Video': emoji_pattern.sub(r'', video.title), 'Views': "{:,}".format(int(video.views)), 'Likes': "{:,}".format(int(video.likes)), 'URL': video.url})
+                writer.writerow({'Channel': channel.author, 'Video': emoji_pattern.sub(r'', video.title), 'Views': "{:,}".format(int(video.views)), 'Likes': "{:,}".format(int(video.likes)), 'URL': video.url, 'Date': video.date})
--- video.py
+++ video.py
@@ -1,5 +1,29 @@
 import requests
 from api_key import API_KEY
+import datetime
+
+# Code from https://stackoverflow.com/questions/410221/natural-relative-days-in-python
+def prettydate(d):
+    diff = datetime.datetime.utcnow() - d
+    s = diff.seconds
+    if diff.days > 7 or diff.days < 0:
+        return d.strftime('%d %b %y')
+    elif diff.days == 1:
+        return '1 day ago'
+    elif diff.days > 1:
+        return '{} days ago'.format(diff.days)
+    elif s <= 1:
+        return 'just now'
+    elif s < 60:
+        return '{} seconds ago'.format(s)
+    elif s < 120:
+        return '1 minute ago'
+    elif s < 3600:
+        return '{} minutes ago'.format(int(s/60))
+    elif s < 7200:
+        return '1 hour ago'
+    else:
+        return '{} hours ago'.format(int(s/3600))
 
 class Video:
     # Static variables
@@ -7,6 +31,7 @@ class Video:
 
     # Object variables
     title = ''
+    date = ''
     views = 0
     likes = 0
     url = ''
@@ -15,6 +40,7 @@ class Video:
         url = Video.VIDEO_STATS_URL + videoId
         response = requests.get(url)
         self.title = response.json()['items'][0]['snippet']['title']
+        self.date = prettydate(datetime.datetime.strptime(response.json()['items'][0]['snippet']['publishedAt'], '%Y-%m-%dT%H:%M:%SZ'))
         self.views = response.json()['items'][0]['statistics']['viewCount']
         self.likes = response.json()['items'][0]['statistics']['likeCount']
         self.url = 'https://www.youtube.com/watch?v=' + videoId
